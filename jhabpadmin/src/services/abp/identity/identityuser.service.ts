844e02c993fa9065bd99c19e11b254f7f92d95ae
caijinhao <531003539@qq.com>
2022-04-14 09:35:30 +0800

    login & userinfo ok

--- jhabpadmin/src/services/abp/identity/identityuser.service.ts
+++ jhabpadmin/src/services/abp/identity/identityuser.service.ts
@@ -1,7 +1,11 @@
 import { request } from 'umi';
 
 export const currentUser = async (): Promise<API.IdentityUser> => {
-  return await request<API.IdentityUser>(`/identityapi/v1/IdentityUser/info`, {
+  const userInfo = await request<API.IdentityUser>(`/identityapi/v1/IdentityUser/info`, {
     method: 'GET',
   });
+  userInfo.avatar =
+    'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png';
+  console.log(userInfo);
+  return userInfo;
 };
