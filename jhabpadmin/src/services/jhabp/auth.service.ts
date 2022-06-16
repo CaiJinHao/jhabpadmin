8159249c358e3dc69471d819e533e8cd5a2218ad
Home-hostpc <531003539@qq.com>
2022-06-16 13:22:33 +0800

    oidc test ok

--- jhabpadmin/src/services/jhabp/auth.service.ts
+++ jhabpadmin/src/services/jhabp/auth.service.ts
@@ -4,43 +4,26 @@ Log.logger = console;
 
 const clientRoot = 'http://localhost:4200/';
 
+// const userManager = new UserManager({
+//   authority: 'https://localhost:6201/',
+//   client_id: 'WebAppYourName_Antd',
+//   redirect_uri: `http://localhost:4200/signin-callback.html`, ///signin-callback.html
+//   // silent_redirect_uri: `${clientRoot}/silent-callback.html`,
+//   post_logout_redirect_uri: `https://localhost:6201/signout-callback-oidc`,
+//   response_type: 'code',
+//   scope: 'email openid profile role phone address WebAppYourName offline_access',
+//   accessTokenExpiringNotificationTime: 300,
+// });
 const userManager = new UserManager({
   authority: 'https://localhost:6201/',
-  client_id: 'WebAppYourName_Antd',
-  redirect_uri: `http://localhost:4200/signin-callback.html`, ///signin-callback.html
+  client_id: 'WebAppYourName_App',
+  redirect_uri: `http://localhost:4200/signin-callback.html`, //
   // silent_redirect_uri: `${clientRoot}/silent-callback.html`,
-  post_logout_redirect_uri: `https://localhost:6201/signout-callback-oidc`,
-  response_type: 'id_token token',
+  post_logout_redirect_uri: `http://localhost:4200`,
+  response_type: 'code',
   scope: 'email openid profile role phone address WebAppYourName offline_access',
   accessTokenExpiringNotificationTime: 300,
 });
-
-userManager.events.addAccessTokenExpiring(async () => {
-  console.log('addAccessTokenExpiring');
-  await renewToken();
-});
-
-userManager.events.addAccessTokenExpired(async () => {
-  console.log('addAccessTokenExpired');
-  await renewToken();
-});
-
-export const getUser = async () => {
-  const authorizationInfo = await userManager.getUser();
-  if (authorizationInfo) {
-    sessionStorage.setItem(AuthorizationInfoStorageKey, JSON.stringify(authorizationInfo));
-    return authorizationInfo;
-  }
-  return await login();
-};
-
-export const login = async () => {
-  return await userManager.signinRedirect();
-};
-
-export const logout = async () => {
-  return await userManager.signoutRedirect();
-};
 
 export const renewToken = async () => {
   const authorizationInfo = await userManager.signinSilent();
@@ -55,6 +38,7 @@ export const renewToken = async () => {
 
 export const getToken = () => {
   const authorizationInfoJson = sessionStorage.getItem(AuthorizationInfoStorageKey);
+  console.log(authorizationInfoJson);
   if (authorizationInfoJson) {
     const authorizationInfo = JSON.parse(authorizationInfoJson);
     return authorizationInfo;
@@ -62,3 +46,30 @@ export const getToken = () => {
     return undefined;
   }
 };
+
+export const login = async () => {
+  return await userManager.signinRedirect();
+};
+
+export const logout = async () => {
+  return await userManager.signoutRedirect();
+};
+
+export const getUser = async () => {
+  const authorizationInfo = await userManager.getUser();
+  if (authorizationInfo) {
+    sessionStorage.setItem(AuthorizationInfoStorageKey, JSON.stringify(authorizationInfo));
+    return authorizationInfo;
+  }
+  return await login();
+};
+
+userManager.events.addAccessTokenExpiring(async () => {
+  console.log('addAccessTokenExpiring');
+  await renewToken();
+});
+
+userManager.events.addAccessTokenExpired(async () => {
+  console.log('addAccessTokenExpired');
+  await renewToken();
+});
