db8405993acb05880110a6a0d7dbe304abb748c0
caijinhao <531003539@qq.com>
2022-07-14 17:41:16 +0800

    add mobile 小程序

--- jhabpmobile/config/index.js
+++ jhabpmobile/config/index.js
@@ -1,6 +1,6 @@
 const config = {
   projectName: 'jhabpmobile',
-  date: '2022-7-13',
+  date: '2022-7-14',
   designWidth: 750,
   deviceRatio: {
     640: 2.34 / 2,
@@ -8,7 +8,7 @@ const config = {
     828: 1.81 / 2
   },
   sourceRoot: 'src',
-  outputRoot: 'dist',
+  outputRoot: `dist/${process.env.TARO_ENV}`,
   plugins: ['@tarojs/plugin-html'],
   defineConstants: {
   },
--- jhabpmobile/src/pages/index/index.tsx
+++ jhabpmobile/src/pages/index/index.tsx
@@ -1,16 +1,13 @@
 import { View, Text } from "@tarojs/components";
-import "./index.less";
+import "./index.scss";
+import { Button } from "@taroify/core";
 
 const Index = () => {
   return (
-    <>
-      <View className='red'>
-        <Text>函数式组件测试!</Text>
-      </View>
-      <div className='red'>
-        <span>Test !</span>
-      </div>
-    </>
+    <View className='index'>
+      <Text>Hello world 666!</Text>
+      <Button color='info'>信息按钮</Button>
+    </View>
   );
 };
 
